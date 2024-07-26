72c87688e3eb31d3f97f726180e7cdcb6e4d9481
hnghia23 <150511971+hnghia23@users.noreply.github.com>
2024-07-26 09:18:46 +0700

    Add files via upload

---
+++ src/main/java/com/learn/identity_service/IdentityServiceApplication.java
@@ -0,0 +1,15 @@
+package com.learn.identity_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class IdentityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityServiceApplication.class, args);
+	}
+
+}
